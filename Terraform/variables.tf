22e717ef421f5821f08f0ddcc1669808123c1888
Alessandro Moura <alessandromoura@hotmail.com>
2020-05-28 23:25:48 +1200

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "Resource group name"
+}
+
+variable "location" {
+  description = "Location"
+}
+
+variable "service_plan_name" {
+  description = "Service plan name"
+}
+
+variable "app_service_name" {
+  description = "App service name"
+}
