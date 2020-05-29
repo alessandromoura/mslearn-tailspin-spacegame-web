5b0857108f7783e893357633365b05a86b10f0d9
Alessandro Moura <alessandromoura@hotmail.com>
2020-05-29 12:41:53 +1200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,6 +16,6 @@ resource "azurerm_app_service_plan" "main" {
 resource "azurerm_app_service" "main" {
   name                = "__app_service_name__"
   location            = "__location__"
-  resource_group_name = "__location__"
+  resource_group_name = "__resource_group_name__"
   app_service_plan_id = "${azurerm_app_service_plan.main.id}"
 }
