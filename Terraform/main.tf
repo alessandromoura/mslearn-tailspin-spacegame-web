e5e7f5acb061ca34f5d76c64aaf068186fd19b9d
Alessandro Moura <alessandromoura@hotmail.com>
2020-05-29 12:12:45 +1200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,8 +23,4 @@ resource "azurerm_app_service" "main" {
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
   app_service_plan_id = "${azurerm_app_service_plan.main.id}"
-  
-  site_config {
-    dotnet_core_version = "v3.1"
-  }
 }
